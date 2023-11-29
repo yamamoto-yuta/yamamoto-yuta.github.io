005dbfdad72a1ea9ba6ebc537f173ca1d568c79b
yamamoto-yuta <yamamoto-yuta@users.noreply.github.com>
2023-11-29 00:29:02 +0000

    Update build to output generated at b9324a7

--- _next/static/2wtRSTAMwYJl55lQL1eOt/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Farticles","\u002Farticles\u002Ftags","\u002F","\u002Fprofile","\u002Fpreview","\u002Farticles\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
